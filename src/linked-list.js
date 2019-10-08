a280ba8bfad35ac99b07a6b37709eefdeb1eb91b
Cl3r1k <Cl3r1k2@gmail.com>
2019-10-08 15:45:03 +0700

    refactor: delete redundant code

--- src/linked-list.js
+++ src/linked-list.js
@@ -13,31 +13,18 @@ class LinkedList {
         let newNodeHead = null;
 
         if (this._tail !== null) {
-            // console.log('this._tail: ', this._tail);
             newNodeHead = this._tail;
         }
 
         const node = new Node(data, newNodeHead);
 
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
         if (this._tail !== null) {
-            // console.log('this._tail: ', this._tail);
-            // console.log('new node: ', node);
-            // console.log('this.length: ', this.length);
-            // console.log('this.doublyLinkedList[this.length][next]: ', this.doublyLinkedList[this.length - 1]);
             this.doublyLinkedList[this.length - 1]['next'] = node;
         }
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
 
         if (!this._head) {
             this._head = node;
         }
-
-        // if (!this.tail) {
-        //     this._tail = node;
-        // } else {
-        //     this._tail = node;
-        // }
 
         this._tail = node;
 
@@ -46,10 +33,6 @@ class LinkedList {
         this.length++;
 
         return this;
-
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
-        // console.log('_head: ', this._head);
-        // console.log('_tail: ', this._tail);
     }
 
     head() {
@@ -61,39 +44,25 @@ class LinkedList {
     }
 
     at(index) {
-        // console.log('index: ', index);
-        // const id = Number(index);
-        // console.log('id: ', id);
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
-        // console.log('this.doublyLinkedList[index]: ', this.doublyLinkedList[index]);
-        // console.log('this.doublyLinkedList[index][data]: ', this.doublyLinkedList[index]['data']);
-        // console.log('this.doublyLinkedList[id]: ', this.doublyLinkedList[id]);
-        // console.log('this.doublyLinkedList[1]: ', this.doublyLinkedList[1]);
-        // return this.doublyLinkedList[index];
         return this.doublyLinkedList[index]['data'];
     }
 
     insertAt(index, data) {
-        // console.log('Object.keys: ', Object.keys(this.doublyLinkedList));
 
         const listKeys = Object.keys(this.doublyLinkedList);
         for (let i = listKeys.length - 1; i >= index; i--) {
-            // console.log('for %s item: ', i, this.doublyLinkedList[i]);
             this.doublyLinkedList[i + 1] = this.doublyLinkedList[i];
             this.doublyLinkedList[i] = {};
         }
         this.length++;
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
 
         let newNodeHead = null;
         let newNodeTail = null;
         if (index > 0) {
             newNodeHead = this.doublyLinkedList[index - 1];
-            // console.log('newNodeHead: ', newNodeHead);
         }
         if (index < this.length - 1) {
             newNodeTail = this.doublyLinkedList[index + 1];
-            // console.log('newNodeTail: ', newNodeTail);
         }
 
         const newNode = new Node(data, newNodeHead, newNodeTail);
@@ -106,8 +75,6 @@ class LinkedList {
             this.doublyLinkedList[index + 1].prev = newNode;
         }
 
-        // console.log('newNode: ', newNode);
-        // console.log('doublyLinkedList NEW+++++: ', this.doublyLinkedList);
         return this;
     }
 
@@ -120,27 +87,16 @@ class LinkedList {
         this._head = null;
         this._tail = null;
         this.length = 0;
-        // console.log('doublyLinkedList: ', this.doublyLinkedList);
-        // console.log('_head: ', this._head);
-        // console.log('_tail: ', this._tail);
-        // console.log('length: ', this.length);
         return this;
     }
 
     deleteAt(index) {
 
-        // console.log('1. deleteAt() BEFORE doublyLinkedList : ', this.doublyLinkedList);
         const listKeys = Object.keys(this.doublyLinkedList);
 
         if (index > 0) {
-            // console.log('index > 0, index: ', index);
             if (this.length > 2) {
-                // console.log('this.length > 2');
-                // console.log('PREV doublyLinkedList[index - 1].next', this.doublyLinkedList[index - 1]);
-                // console.log('PREV NEW doublyLinkedList[index + 1]', this.doublyLinkedList[index + 1]);
-                // console.log('doublyLinkedList[i - 1].next', this.doublyLinkedList[i - 1].next);
                 this.doublyLinkedList[index - 1].next = this.doublyLinkedList[index + 1];
-                // console.log('NEW NEXT doublyLinkedList[index - 1].next', this.doublyLinkedList[index - 1]);
             } else {
                 this.doublyLinkedList[index - 1].next = null;
             }
@@ -148,23 +104,17 @@ class LinkedList {
 
         if (index < this.length - 1) {
             if (index > 0) {
-                // console.log('NEXT doublyLinkedList[index + 1].next', this.doublyLinkedList[index + 1]);
-                // console.log('NEXT NEW doublyLinkedList[index - 1]', this.doublyLinkedList[index - 1]);
                 this.doublyLinkedList[index + 1].prev = this.doublyLinkedList[index - 1];
-                // console.log('NEW PREV doublyLinkedList[index - 1].prev', this.doublyLinkedList[index + 1]);
             } else {
                 this.doublyLinkedList[index + 1].prev = null;
             }
         } // else consider as deleted Last node
 
         for (let i = index; i < listKeys.length - 1; i++) {
-            // console.log('for %s item: ', i, this.doublyLinkedList[i]);
             this.doublyLinkedList[i] = this.doublyLinkedList[i + 1];
         }
-        // console.log('2. deleteAt() BEFORE doublyLinkedList : ', this.doublyLinkedList);
         delete this.doublyLinkedList[this.length];
         this.length--;
-        // console.log('3. deleteAt() doublyLinkedList : ', this.doublyLinkedList);
 
         return this;
     }
@@ -175,29 +125,12 @@ class LinkedList {
         const newDoublyLinkedList = {};
         let y = 0;
         for (let i = listKeys.length - 1; i >= 0; i--, y++) {
-            // console.log('1. for index[%s] item: ', i, this.doublyLinkedList[i]);
-            // if (i === listKeys.length - 1) {
-            //     console.log('1.1 i === listKeys.length - 1');
-            //     this._head = this.doublyLinkedList[i];
-            //     this.doublyLinkedList[i].prev = null;
-            // }
-            // if (i === 0) {
-            //     console.log('1.2 i === 0');
-            //     this._tail = this.doublyLinkedList[i];
-            //     console.log('1.2.1 this._tail', this._tail);
-            //     this.doublyLinkedList[i].next = null;
-            // }
 
             newDoublyLinkedList[y] = this.doublyLinkedList[i];
-            // console.log('2. newDoublyLinkedList[%s]: ', y, newDoublyLinkedList[y]);
-            // if (i - 1 > 0) {
-            //     newDoublyLinkedList[y].next = this.doublyLinkedList[i - 1];
-            // }
             if (y - 1 >= 0) {
                 newDoublyLinkedList[y].prev = newDoublyLinkedList[y - 1];
                 newDoublyLinkedList[y - 1].next = newDoublyLinkedList[y];
             }
-            // console.log('3. newDoublyLinkedList[%s] updated: ', y, newDoublyLinkedList[y]);
         }
 
         newDoublyLinkedList[0].prev = null;
@@ -206,8 +139,6 @@ class LinkedList {
         this._head = newDoublyLinkedList[0];
         this._tail = newDoublyLinkedList[listKeys.length - 1];
 
-        // console.log('1. this.doublyLinkedList: ', this.doublyLinkedList);
-        // console.log('2. newDoublyLinkedList: ', newDoublyLinkedList);
         this.doublyLinkedList = newDoublyLinkedList;
 
         return this;
